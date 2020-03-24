c1e7a174eca2039fd0ced4b946e9bd49c5244b82
Marius Erath <marius.erath@gmx.de>
2020-03-24 19:32:32 +0100

    Implemented basic main menu

---
+++ App/app/src/main/java/com/example/phone_remote_app/RemoteActivity.java
@@ -0,0 +1,17 @@
+package com.example.phone_remote_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RemoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remote);
+    }
+}
