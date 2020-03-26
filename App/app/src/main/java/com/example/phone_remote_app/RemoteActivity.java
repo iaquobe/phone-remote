494ac7f54b012017b0d085224ea6fee82b6e8cdb
Marius Erath <marius.erath@gmx.de>
2020-03-26 21:41:05 +0100

    Added: -Touch tracking -Basic Connection Interface

--- App/app/src/main/java/com/example/phone_remote_app/RemoteActivity.java
+++ App/app/src/main/java/com/example/phone_remote_app/RemoteActivity.java
@@ -2,16 +2,39 @@ package com.example.phone_remote_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
+import android.view.MotionEvent;
 
 public class RemoteActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+        String location = "m " + x + " " + y + "\n";
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.i("TAG", "touched down");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.i("TAG", location);
+                //TODO sent data
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.i("TAG", "touched up");
+                break;
+        }
+
+        return true;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remote);
+
     }
 }
