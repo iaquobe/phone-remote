b1132b2037dbfd6c1ea081ca49702e5943add6cb
Jakob <jakob.lamberthartmann.canada@gmail.com>
2020-03-31 14:37:51 +0200

    mouse offset

--- test/client.py
+++ test/client.py
@@ -1,13 +1,13 @@
 import socket
 
-HOST = '127.0.0.1'
+HOST = ''
 PORT = 9000
 
 def test():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         while True:
-            text = "m {}\n".format(input())
+            text = "{}\n".format(input())
             bt = bytearray(text, "utf-8")
             s.sendall(bt)
 
